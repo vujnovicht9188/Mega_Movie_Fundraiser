994741385c39c524a224add045e81525f172139c
vujnovicht9188 <78515942+vujnovicht9188@users.noreply.github.com>
2022-02-18 12:36:50 +1300

    Add files via upload

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -1,44 +1,11 @@
-# functions
+import random 
 
-from tkinter.tix import MAX
+answer_list = ["ofc", "no you're dumb", "dude just stop talking", "maybe", "good question", "i don't really know", "lol"]
 
+while 1 == 1:  
+    question = input("Hurry up, ask me a question ----> ")
 
-def not_blank(question, error): 
-    valid = False
-
-    while not valid:
-        response = input(question)
-
-        if response !="":
-            return response
-        else:
-            print(error)
-
-
-# start of loop
-
-# initialise loop so that is runs at least once
-name = ""
-count = 0
-MAX_TICKETS = 5
-
-while name != "xxx" and count < MAX_TICKETS:
-
-    # get details
-    name = not_blank("name: ", "<error> name cannot be blank")
-    if name != 'xxx':  
-        count += 1
-        if count == 4:
-            print("you have only 1 seat left!")
-        else:   
-            print ("you have {} seats "
-            "left".format(MAX_TICKETS - count))
-
-        print()
-
-if count == MAX_TICKETS:
-    print("You have sold all the available tickets!")
-else :
-    print("You have sold {} tickets. \n"
-    "There are {} places still available"
-    .format(count, MAX_TICKETS - count))
+    if question != "xxx":
+        print(random.choice(answer_list))
+    else:
+        break
