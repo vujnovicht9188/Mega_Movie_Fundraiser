a6e230a941b969421cb949de62da799a1a47562f
vujnovicht9188 <78515942+vujnovicht9188@users.noreply.github.com>
2022-02-23 09:06:42 +1300

    04_number_checker

---
+++ 04_number_checker.py
@@ -0,0 +1,29 @@
+
+
+def int_check(question, low_num, high_num):
+
+    error = "Please enter a whole number between {} "\
+            "and {}".format(low_num,high_num)
+
+    valid = False
+
+    while not valid:
+
+        # askuser for number an check it is valid
+        try:
+            response = int(input(question))
+
+            if low_num <= response <= high_num:
+                return response
+            else: 
+                print(error)     
+
+        # if an integer is not entered display an error
+        except ValueError:
+            print(error)
+
+
+# main routine goes here
+age = int_check("Age: ", 12, 130)
+
+
